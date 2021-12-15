f964efa12c42a183aa1f0111bd90ca7dfaf51862
west1636 <nishikawa@nii.ac.jp>
2021-12-15 09:13:04 +0900

    add integromat test code

---
+++ addons/integromat/tests/test_models.py
@@ -0,0 +1,75 @@
+# from nose.tools import *  # noqa
+import unittest
+
+import mock
+import pytest
+from nose.tools import (assert_false, assert_true,
+                        assert_equal, assert_is_none)
+
+from addons.base.tests.models import (
+    OAuthAddonNodeSettingsTestSuiteMixin,
+    OAuthAddonUserSettingTestSuiteMixin
+)
+from addons.integromat import SHORT_NAME, FULL_NAME
+from addons.integromat import settings
+from addons.integromat.models import NodeSettings, Attendees
+from addons.integromat.tests.factories import (
+    IntegromatUserSettingsFactory,
+    IntegromatNodeSettingsFactory,
+    IntegromatAccountFactory,
+    IntegromatAttendeesFactory
+)
+from framework.auth import Auth
+from osf_tests.factories import ProjectFactory, DraftRegistrationFactory
+from tests.base import get_default_metaschema
+
+pytestmark = pytest.mark.django_db
+
+
+class TestUserSettings(OAuthAddonUserSettingTestSuiteMixin, unittest.TestCase):
+
+    short_name = SHORT_NAME
+    full_name = FULL_NAME
+    ExternalAccountFactory = IntegromatAccountFactory
+
+
+class TestNodeSettings(OAuthAddonNodeSettingsTestSuiteMixin, unittest.TestCase):
+
+    short_name = SHORT_NAME
+    full_name = FULL_NAME
+    ExternalAccountFactory = IntegromatAccountFactory
+    NodeSettingsFactory = IntegromatNodeSettingsFactory
+    NodeSettingsClass = NodeSettings
+    UserSettingsFactory = IntegromatUserSettingsFactory
+
+    def test_registration_settings(self):
+        registration = ProjectFactory()
+        clone, message = self.node_settings.after_register(
+            self.node, registration, self.user,
+        )
+        assert_is_none(clone)
+
+    def test_before_register_no_settings(self):
+        self.node_settings.user_settings = None
+        message = self.node_settings.before_register(self.node, self.user)
+        assert_false(message)
+
+    def test_before_register_no_auth(self):
+        self.node_settings.external_account = None
+        message = self.node_settings.before_register(self.node, self.user)
+        assert_false(message)
+
+    def test_before_register_settings_and_auth(self):
+        message = self.node_settings.before_register(self.node, self.user)
+        assert_true(message)
+
+@pytest.mark.django_db
+def test_default_guest_user_guid():
+    inst = integromatAttendeesFactory(fullname='TEST USER', is_guest=True)
+    result = Attendees.objects.filter(fullname='TEST USER')
+    expected_user_guid = None
+    expected_microsoft_teams_mail = None
+    expected_webex_meetings_mail = None
+    assert_equals(result.user_guid, expected_user_guid)
+    assert_equals(result.microsoft_teams_user_name, expected_webex_meetings_mail)
+    assert_equals(result.expected_webex_meetings_mail, expected_webex_meetings_mail)
