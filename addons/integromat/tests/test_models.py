59279c576bdc435397a1e34b2992d5200cba25f7
west1636 <nishikawa@nii.ac.jp>
2021-12-16 16:38:56 +0900

    add temp log , revise assert_equal

--- addons/integromat/tests/test_models.py
+++ addons/integromat/tests/test_models.py
@@ -94,6 +94,6 @@ def test_default_guest_user_guid():
     expected_user_guid = None
     expected_microsoft_teams_mail = None
     expected_webex_meetings_mail = None
-    assert_equal(rJson.fields.user_guid, expected_user_guid)
-    assert_equal(rJson.fields.microsoft_teams_user_name, expected_webex_meetings_mail)
-    assert_equal(rJson.fields.webex_meetings_mail, expected_webex_meetings_mail)
+    assert_equal(rJson['fields']['user_guid'], expected_user_guid)
+    assert_equal(rJson.['fields'].['microsoft_teams_user_name'], expected_webex_meetings_mail)
+    assert_equal(rJson.['fields']['webex_meetings_mail'], expected_webex_meetings_mail)
