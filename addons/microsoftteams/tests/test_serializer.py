580fb85f38a00b4ea92240be640452bcde5152c0
west1636 <nishikawa@nii.ac.jp>
2022-09-09 21:43:47 +0900

    revise test_serializer.py

--- addons/microsoftteams/tests/test_serializer.py
+++ addons/microsoftteams/tests/test_serializer.py
@@ -4,23 +4,23 @@ import mock
 import pytest
 
 from addons.base.tests.serializers import StorageAddonSerializerTestSuiteMixin
-from addons.microsoftteams.tests.factories import MicrosoftTteamsAccountFactory
-from addons.microsoftteams.serializer import MicrosoftTteamsSerializer
+from addons.microsoftteams.tests.factories import MicrosoftTeamsAccountFactory
+from addons.microsoftteams.serializer import MicrosoftTeamsSerializer
 
 from tests.base import OsfTestCase
 
 pytestmark = pytest.mark.django_db
 
-class TestMicrosoftTteamsSerializer(StorageAddonSerializerTestSuiteMixin, OsfTestCase):
+class TestMicrosoftTeamsSerializer(StorageAddonSerializerTestSuiteMixin, OsfTestCase):
     addon_short_name = 'microsoftteams'
-    Serializer = MicrosoftTteamsSerializer
-    ExternalAccountFactory = MicrosoftTteamsAccountFactory
+    Serializer = MicrosoftTeamsSerializer
+    ExternalAccountFactory = MicrosoftTeamsAccountFactory
 
     def set_provider_id(self, pid):
         self.node_settings.folder_id = pid
 
     def setUp(self):
-        super(TestMicrosoftTteamsSerializer, self).setUp()
+        super(TestMicrosoftTeamsSerializer, self).setUp()
 
     def tearDown(self):
-        super(TestMicrosoftTteamsSerializer, self).tearDown()
+        super(TestMicrosoftTeamsSerializer, self).tearDown()
